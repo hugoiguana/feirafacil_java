951885750e6d63df42f0db32e46179b253356b76
hugoiguana <hugo.iguanaa@gmail.com>
2019-09-11 23:08:45 -0300

    Config router and login component.

--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes : Routes = [
+  {
+    path : '',
+    component : LoginComponent
+  }
+  /* {
+    path : '**',
+    component : PageNotFoundComponent
+  } */
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing : true })
   ],
   providers: [],
   bootstrap: [AppComponent]
